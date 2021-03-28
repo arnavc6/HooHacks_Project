4aad35753119412954190ed7cd25e04a5ecf609d
Maggie <77594458+mtran4@users.noreply.github.com>
2021-03-27 23:10:56 -0400

    Skeleton for game

--- Game.py
+++ Game.py
@@ -13,7 +13,20 @@ p_armor = 0
 p_rounds = 0
 p_points = 0
 
-monst = Monster(m_hp, m_damage, m_toughness, m_pointvalue)
-player1 = Player(p_hp, p_damage, p_armor, p_rounds, p_points)
-monst.intro()
-player1.desc()
+def controlgame():
+    print("Press 1 to start game, 0 to quit")
+    game_state = int(input())
+    if game_state == 1:
+        monst = Monster(m_hp, m_damage, m_toughness, m_pointvalue)
+        player1 = Player(p_hp, p_damage, p_armor, p_rounds, p_points)
+        monst.intro()
+        print("Your stats: ")
+        player1.desc()
+        print("Monster's stats")
+        monst.desc()
+    while game_state != 0 and monst.hp != 0 and player1.hp != 0:
+        #call fighting function
+    print("Game over!")
+
+#Start the game
+controlgame()
